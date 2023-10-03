f3472f9f971b2e4d21120ac491d19cb8a53328e4
bnasare <124492150+bnasare@users.noreply.github.com>
2023-10-03 22:08:36 +0000

    remove

--- Doubly Linked List Methods/DLL_Remove.py
+++ Doubly Linked List Methods/DLL_Remove.py
@@ -2,9 +2,10 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+        self.prev = None
 
 
-class LinkedList:
+class DoublyLinkedList:
     def __init__(self, value):
         new_node = Node(value)
         self.head = new_node
@@ -19,11 +20,12 @@ class LinkedList:
 
     def append(self, value):
         new_node = Node(value)
-        if self.length == 0:
+        if self.head is None:
             self.head = new_node
             self.tail = new_node
         else:
             self.tail.next = new_node
+            new_node.prev = self.tail
             self.tail = new_node
         self.length += 1
         return True
@@ -31,17 +33,15 @@ class LinkedList:
     def pop(self):
         if self.length == 0:
             return None
-        temp = self.head
-        pre = self.head
-        while(temp.next):
-            pre = temp
-            temp = temp.next
-        self.tail = pre
-        self.tail.next = None
-        self.length -= 1
-        if self.length == 0:
+        temp = self.tail
+        if self.length == 1:
             self.head = None
             self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+        self.length -= 1
         return temp
 
     def prepend(self, value):
@@ -51,6 +51,7 @@ class LinkedList:
             self.tail = new_node
         else:
             new_node.next = self.head
+            self.head.prev = new_node
             self.head = new_node
         self.length += 1
         return True
@@ -59,19 +60,27 @@ class LinkedList:
         if self.length == 0:
             return None
         temp = self.head
-        self.head = self.head.next
-        temp.next = None
-        self.length -= 1
-        if self.length == 0:
+        if self.length == 1:
+            self.head = None
             self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
         return temp
 
     def get(self, index):
         if index < 0 or index >= self.length:
             return None
         temp = self.head
-        for _ in range(index):
-            temp = temp.next
+        if index < self.length/2:
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
         return temp
 
     def set_value(self, index, value):
@@ -88,58 +97,55 @@ class LinkedList:
             return self.prepend(value)
         if index == self.length:
             return self.append(value)
+
         new_node = Node(value)
-        temp = self.get(index - 1)
-        new_node = Node
-        new_node.next = temp.next
-        temp.next = new_node
+        before = self.get(index - 1)
+        after = before.next
+
+        new_node.prev = before
+        new_node.next = after
+        before.next = new_node
+        after.prev = new_node
+
         self.length += 1
         return True
 
     def remove(self, index):
-    # Check if index is out of bounds
         if index < 0 or index >= self.length:
             return None
-    # Remove and return the first node
         if index == 0:
             return self.pop_first()
-    # Remove and return the last node
-        if index == self.length - 1:
+        if index == self.length-1:
             return self.pop()
-    # Get the previous node
-        pre = self.get(index - 1)
-    # Set temp to the node to be removed
-        temp = pre.next
-    # Update pre.next to skip the removed node
-        pre.next = temp.next
-    # Disconnect the removed node
+        temp = self.get(index)
+        temp.prev.next = temp.next
+        temp.next.prev = temp.prev
         temp.next = None
-    # Decrement the list length
+        temp.prev = None
         self.length -= 1
-    # Return the removed node
         return temp
 
 
-my_linked_list = LinkedList(1)
-my_linked_list.append(2)
-my_linked_list.append(3)
-my_linked_list.append(4)
-my_linked_list.append(5)
+my_doubly_linked_list = DoublyLinkedList(1)
+my_doubly_linked_list.append(2)
+my_doubly_linked_list.append(3)
+my_doubly_linked_list.append(4)
+my_doubly_linked_list.append(5)
 
-print('LL before remove():')
-my_linked_list.print_list()
-
-print('\nRemoved node:')
-print(my_linked_list.remove(2).value)
-print('LL after remove() in middle:')
-my_linked_list.print_list()
+print('DLL before remove():')
+my_doubly_linked_list.print_list()
 
 print('\nRemoved node:')
-print(my_linked_list.remove(0).value)
-print('LL after remove() of first node:')
-my_linked_list.print_list()
+print(my_doubly_linked_list.remove(2).value)
+print('DLL after remove() in middle:')
+my_doubly_linked_list.print_list()
 
 print('\nRemoved node:')
-print(my_linked_list.remove(2).value)
-print('LL after remove() of last node:')
-my_linked_list.print_list()
+print(my_doubly_linked_list.remove(0).value)
+print('DLL after remove() of first node:')
+my_doubly_linked_list.print_list()
+
+print('\nRemoved node:')
+print(my_doubly_linked_list.remove(2).value)
+print('DLL after remove() of last node:')
+my_doubly_linked_list.print_list()
