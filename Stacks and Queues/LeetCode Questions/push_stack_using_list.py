24431ea3d1d89b398ff9c14db3f57c83f1e77ee6
bnasare <124492150+bnasare@users.noreply.github.com>
2023-10-05 15:46:46 +0000

    stack push list

--- Stacks and Queues/LeetCode Questions/push_stack_using_list.py
+++ Stacks and Queues/LeetCode Questions/push_stack_using_list.py
@@ -1,6 +1,6 @@
 class Stack:
     def __init__(self):
-        self.stack_list=[]
+        self.stack_list = []
 
     def print_stack(self):
         for values in self.stack_list:
@@ -9,7 +9,8 @@ class Stack:
     def push(self, value):
         self.stack_list.append(value)
 
-my_stack=Stack()
+
+my_stack = Stack()
 my_stack.push(1)
 my_stack.push(2)
 my_stack.push(3)
