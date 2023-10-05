6664433e7dffda9761dd7ef0dbef9b587f152413
bnasare <124492150+bnasare@users.noreply.github.com>
2023-10-05 13:42:46 +0000

    pop stack

--- Stacks and Queues/Stacks/Methods/Stack_Pop.py
+++ Stacks and Queues/Stacks/Methods/Stack_Pop.py
@@ -24,14 +24,31 @@ class Stack:
             new_node.next = self.top
             self.top = new_node
         self.height += 1
+        return True
+
+    def pop(self):
+        if self.height == 0:
+            return None
+        temp = self.top
+        if self.height == 1:
+            self.top = None
+        else:
+            self.top = temp.next
+            temp.next = None
+        self.height -= 1
+        return temp
 
 
-my_stack = Stack(2)
-
-print('Stack before push(1):')
-my_stack.print_stack()
-
+my_stack = Stack(4)
+my_stack.push(3)
+my_stack.push(2)
 my_stack.push(1)
 
-print('\nStack after push(1):')
+print('Stack before pop():')
+my_stack.print_stack()
+
+print('\nPopped node:')
+print(my_stack.pop().value)
+
+print('\nStack after pop():')
 my_stack.print_stack()
